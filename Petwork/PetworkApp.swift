5b6700c0f9a4245c422533acc391bcec7c9a4017
Meher Khurana <createapp1234@gmail.com>
2023-12-23 19:21:09 -0800

    Initial Commit

---
+++ Petwork/PetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetworkApp.swift
+//  Petwork
+//
+//  Created by Meher Khurana on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
